faee18e056f5186648d6b9a59f6d9b67d82c37db
Ihor_Stasovskyi <stsovsky400m@gmail.com>
2022-05-11 22:27:35 +0200

    relation between models and create url

--- people/admin.py
+++ people/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from.models import Woman, Category
 
-# Register your models here.
+admin.site.register(Woman)
+admin.site.register(Category)
--- people/views.py
+++ people/views.py
@@ -1,31 +1,62 @@
-from django.http import HttpResponse, HttpResponseNotFound
+from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import render, redirect
-from .models import Woman
+from .models import Woman, Category
 
-menu = ['O stronie', 'Dodać artykuł', 'Kontakt', 'Wejść']
+menu = [{'title': 'O stronie', 'url_name': 'about'},
+        {'title': 'Dodać artykuł', 'url_name': 'add_page'},
+        {'title': 'Kontakt', 'url_name': 'contact'},
+        {'title': 'Wejść', 'url_name': 'login'}]
 
 
 def index(request):
     posts = Woman.objects.all()
-    return render(request, 'people/index.html', {'title': 'Główna strona', 'menu': menu, 'woman': posts})
+    cats = Category.objects.all()
+    ctx = {
+        'menu': menu,
+        'woman': posts,
+        'cats': cats,
+        'title': 'Strona główna',
+        'cat_selected': 0,
+    }
+    return render(request, 'people/index.html', context=ctx)
 
 
 def about(request):
     return render(request, 'people/about.html', {'title': 'O co chodzi', 'menu': menu})
 
 
-def categories(request, cat_id):
-    if (request.GET):
-        print(request.GET)
-    return HttpResponse(f'<h1>Artykuły po kategoriach</h1><p>{cat_id}</p>')
-
-
-def archive(request, year):
-    if int(year) > 2020:
-        return redirect('home', permanent=True)
-    return HttpResponse(f'Rok {year}')
-
-
 def pageNotFound(request, exception):
     return HttpResponseNotFound('<h1>Strona nie odnaleziona</h1>')
 
+
+def addpage(request):
+    return HttpResponse('Dodawanie strony')
+
+
+def login(request):
+    return HttpResponse('Autoryzacja')
+
+
+def contact(request):
+    return HttpResponse('Kontakt')
+
+
+def show_post(request, post_id):
+    return HttpResponse(f'Prezentacja artykuła z id = {post_id}')
+
+
+def show_category(request, cat_id):
+    posts = Woman.objects.filter(cat_id=cat_id)
+    cats = Category.objects.all()
+
+    if len(posts) == 0:
+        raise Http404()
+
+    ctx = {
+        'menu': menu,
+        'woman': posts,
+        'cats': cats,
+        'title': 'Pokaz po kategoriam',
+        'cat_selected': cat_id,
+    }
+    return render(request, 'people/index.html', context=ctx)
